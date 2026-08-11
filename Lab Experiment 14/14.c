d730427839fb0e5dab27426f9c386907e83db917
Nokeshwaran27 <192511007.simats@saveetha.com>
2026-08-11 20:53:33 +0530

    Add files via upload

---
+++ Lab Experiment 14/14.c
@@ -0,0 +1,17 @@
+#include <reg51.h>
+#include <stdio.h>
+
+void main(void)
+{
+    SCON = 0x50;      /* SCON: mode 1, 8-bit UART, enable rcvr */
+    TMOD = 0x20;      /* TMOD: Timer 1, mode 2, 8-bit auto reload */
+    TH1  = 0xFD;      /* TH1: reload value for 9600 baud rate */
+
+    TR1 = 1;          /* TR1: Timer 1 run */
+    TI  = 1;          /* TI: Set TI to send first character of UART */
+
+    while (1)
+    {
+        printf("Hello World ! \n");
+    }
+}
